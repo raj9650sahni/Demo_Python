855b7c9dbf3eb3c324192b54469715df5dadabad
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-06-18 14:59:30 +0530

    added Dictionary

--- new.py
+++ new.py
@@ -4,3 +4,13 @@ list.append(2)
 list.append(3)
 for s in list:
     print(s)
+
+
+#uc_2
+
+dict = {}
+for i in range(len(list)):
+    dict[i] = list[i]
+
+for key,values in dict.items():
+    print(key,values)
